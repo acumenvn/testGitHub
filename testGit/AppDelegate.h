e8fa5e5ad55ae5daa5c56d6c186e7c7ca22c1fb5
acumenvn <acumen.vn@gmail.com>
2013-09-05 10:07:22 +0700

    Initial Commit

---
+++ testGit/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by Tri Vo on 9/5/13.
+//  Copyright (c) 2013 Tri Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ testGit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testGit
+//
+//  Created by Tri Vo on 9/5/13.
+//  Copyright (c) 2013 Tri Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
